4f93bca6c0ff835fa520115dad641c282424ea1c
wongbingg <wb277@naver.com>
2024-05-12 15:55:28 +0900

    feat : DB 연결

--- member/src/main/java/com/example/member/MemberApplication.java
+++ member/src/main/java/com/example/member/MemberApplication.java
@@ -2,8 +2,10 @@ package com.example.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//		(exclude={DataSourceAutoConfiguration.class}) // () 안에 DB를 사용하지 않겠다는 설정
 public class MemberApplication {
 
 	public static void main(String[] args) {
